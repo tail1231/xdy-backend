28ab91984bea721dece983fca9b88cef283cb941
mingyu.chen <cdy1571@163.com>
2025-08-19 21:41:28 +0800

    feat:添加vercel配置

--- src/index.ts
+++ src/index.ts
@@ -18,9 +18,9 @@ app.use(orderRoutes.routes()).use(orderRoutes.allowedMethods());
 app.use(userRoutes.routes()).use(userRoutes.allowedMethods());
 app.use(customerRoutes.routes()).use(customerRoutes.allowedMethods());
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
 
 export default app;
